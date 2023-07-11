742f57a50be5e755409aa54a90fb662238c07052
minkyu-types <lion9638@naver.com>
2023-07-11 17:15:29 +0900

    2023-07-11 | mkkim | Compose 세팅 수정, Home 화면 수정

--- presentation/src/main/java/com/kyutypes/app/presentation/MainActivity.kt
+++ presentation/src/main/java/com/kyutypes/app/presentation/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.kyutypes.app.presentation
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,7 +25,6 @@ import androidx.navigation.compose.rememberNavController
 import com.kyutypes.app.presentation.ui.theme.Convert
 import com.kyutypes.app.presentation.ui.theme.Home
 import com.kyutypes.app.presentation.ui.theme.Result
-import com.kyutypes.app.presentation.ui.theme.Screen
 import com.kyutypes.app.presentation.ui.theme.TimeMagicianTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,9 +40,37 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LayoutHome() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Time Magician")
+                },
+                actions = {
+                    IconButton(onClick = { doSomething() }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
 
+fun doSomething() {
+    Log.e("Do something ya", "H A L A")
+}
+
+@Composable
+fun BodyContent(modifier: Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks!")
+    }
 }
 
 @Preview
