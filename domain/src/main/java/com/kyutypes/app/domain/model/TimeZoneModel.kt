f28d3b4857b6dc19938cc1a448786e879fac3c2e
minkyu-types <lion9638@naver.com>
2023-07-17 15:44:38 +0900

    2023-07-17 | mkkim | TimeZone Domain 계층 코드 삭제, History에서 이력 관리하기로 결정

--- domain/src/main/java/com/kyutypes/app/domain/model/TimeZoneModel.kt
+++
@@ -1,6 +0,0 @@
-package com.kyutypes.app.domain.model
-
-data class TimeZoneModel(
-    val name: String,
-    val abbreviation: String
-)
--- domain/src/main/java/com/kyutypes/app/domain/repository/TimeZoneRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.kyutypes.app.domain.repository
-
-import com.kyutypes.app.domain.model.TimeZoneModel
-
-interface TimeZoneRepository {
-    fun getSavedTimeZone(): TimeZoneModel?
-    fun saveTimeZone(timezone: TimeZoneModel)
-    fun convertTimeZone(source: TimeZoneModel, target: TimeZoneModel)
-}
