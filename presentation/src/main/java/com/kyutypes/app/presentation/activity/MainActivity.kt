f34ffcb49bfe8bb9a77494ce35df9e488eda1eb4
minkyu-types <lion9638@naver.com>
2023-07-24 14:52:34 +0900

    2023-07-24 | mkkim | Compose 드랍, xml 재도입

--- presentation/src/main/java/com/kyutypes/app/presentation/activity/MainActivity.kt
+++ presentation/src/main/java/com/kyutypes/app/presentation/activity/MainActivity.kt
@@ -3,16 +3,6 @@ package com.kyutypes.app.presentation.activity
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.kyutypes.app.presentation.ui.screen.TabLayout
-import com.kyutypes.app.presentation.ui.theme.TimeMagicianTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,27 +10,5 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TimeMagicianTheme {
-                Scaffold(
-
-                ) {
-                    TabLayout()
-                }
-            }
-        }
-    }
-}
-
-@Preview
-@Composable
-fun LayoutHomePreview() {
-    TimeMagicianTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            TabLayout()
-        }
     }
 }
