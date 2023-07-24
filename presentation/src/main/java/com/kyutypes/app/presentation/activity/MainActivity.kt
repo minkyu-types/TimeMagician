5fd1271067a474bff205efb9dbe24f6c971ffa42
minkyu-types <lion9638@naver.com>
2023-07-24 16:25:55 +0900

    2023-07-24 | mkkim | BaseFragment 추가, MainActivity + NavGraph

--- presentation/src/main/java/com/kyutypes/app/presentation/activity/MainActivity.kt
+++ presentation/src/main/java/com/kyutypes/app/presentation/activity/MainActivity.kt
@@ -1,13 +1,44 @@
 package com.kyutypes.app.presentation.activity
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.kyutypes.app.presentation.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private val navController by lazy {
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
---
+++ presentation/src/main/java/com/kyutypes/app/presentation/fragment/BaseFragment.kt
@@ -0,0 +1,38 @@
+package com.kyutypes.app.presentation.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.viewbinding.ViewBinding
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+abstract class BaseFragment<T: ViewBinding>: Fragment() {
+    private var _binding: T?= null
+    protected val binding get() = _binding!!
+
+    private var _compositeDisposable: CompositeDisposable? = null
+    protected val compositeDisposable get() = _compositeDisposable!!
+
+    protected val navController: NavController by lazy { findNavController() }
+
+    abstract fun getFragmentBinding(inflater: LayoutInflater, container: ViewGroup?): T
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = getFragmentBinding(inflater, container)
+        _compositeDisposable = CompositeDisposable()
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
