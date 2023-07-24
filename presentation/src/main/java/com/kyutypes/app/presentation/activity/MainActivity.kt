f7f4192f50b3d5fe24280900809b69cc09b8ff72
minkyu-types <lion9638@naver.com>
2023-07-24 17:15:07 +0900

    2023-07-24 | mkkim | DataBinding 설정 수정, BottomNavigation 추가

--- presentation/src/main/java/com/kyutypes/app/presentation/activity/MainActivity.kt
+++ presentation/src/main/java/com/kyutypes/app/presentation/activity/MainActivity.kt
@@ -2,20 +2,34 @@ package com.kyutypes.app.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.kyutypes.app.presentation.R
+import com.kyutypes.app.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private val navController by lazy {
-        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-    }
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding!!
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        navController.addOnDestinationChangedListener{
+                _, destination, _ ->
+            when(destination.id){
+
+            }
+        }
+//        binding.bottomNavigation.setupWithNavController(navController)
     }
 
     override fun onStart() {
