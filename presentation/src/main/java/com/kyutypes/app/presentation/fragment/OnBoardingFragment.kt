048b80677ff32ff659284824ad2aef07a74c8c7f
minkyu-types <lion9638@naver.com>
2023-07-27 17:40:28 +0900

    2023-07-27 | mkkim | onBoardingFragment 수정

--- presentation/src/main/java/com/kyutypes/app/presentation/fragment/OnBoardingFragment.kt
+++ presentation/src/main/java/com/kyutypes/app/presentation/fragment/OnBoardingFragment.kt
@@ -1,20 +1,13 @@
 package com.kyutypes.app.presentation.fragment
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.viewModels
-import androidx.viewpager2.widget.ViewPager2
 import com.kyutypes.app.presentation.databinding.FragmentOnBoardingBinding
 import com.kyutypes.app.presentation.viewmodel.OnBoardingViewModel
 
 class OnBoardingFragment private constructor(): BaseFragment<FragmentOnBoardingBinding>() {
     private val viewModel: OnBoardingViewModel by viewModels()
-
-    private lateinit var viewPager: ViewPager2
-    private lateinit var nextButton: Button
 
     companion object {
         fun newInstance() = OnBoardingFragment()
