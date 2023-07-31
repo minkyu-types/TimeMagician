912ce5d88c32f7993e3ffa079dcd3034391c0905
minkyu-types <lion9638@naver.com>
2023-07-31 16:58:54 +0900

    2023-07-31 | mkkim | ConvertLayout 수정

--- presentation/src/main/java/com/kyutypes/app/presentation/fragment/ConvertFragment.kt
+++ presentation/src/main/java/com/kyutypes/app/presentation/fragment/ConvertFragment.kt
@@ -1,6 +1,8 @@
 package com.kyutypes.app.presentation.fragment
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.kyutypes.app.presentation.databinding.FragmentConvertBinding
@@ -18,5 +20,41 @@ class ConvertFragment : BaseFragment<FragmentConvertBinding>() {
         container: ViewGroup?
     ): FragmentConvertBinding {
         return FragmentConvertBinding.inflate(inflater, container, false)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
